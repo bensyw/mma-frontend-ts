967652eedc8ffb40614f9796d46fb0ec592bcb5e
Shengyu Wang <shengyuwang95@gmail.com>
2021-02-20 15:52:04 +1100

    Add a themed QueryButton

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ComboBox } from "../ComboBox";
+import { QueryButton } from "../QueryButton";
 
 export const Container: React.FunctionComponent<{}> = () => {
     return (
         <>
             <ComboBox />
             <ComboBox />
+            <QueryButton />
         </>
     )
 }
